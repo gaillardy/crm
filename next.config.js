c256203aade11effb25148253511ed0730875457
Yohan <geektateur263@gmail.com>
2025-06-25 23:41:15 +0300

    Installation de tout les packages nécessaire et configuration de Tailwind

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: { 
+    unoptimized: true,
+    domains: ['images.pexels.com']
+  },
+  experimental: {
+    optimizePackageImports: ['lucide-react']
+  }
+};
+
+module.exports = nextConfig;
